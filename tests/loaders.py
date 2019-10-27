4cdf278feea26cbfb5b62b4331e99a0493ce9d9a
Dylan Bruce <dylankbruce@gmail.com>
2019-10-27 10:42:00 -0400

    fix?

--- tests/loaders.py
+++ tests/loaders.py
@@ -6,7 +6,7 @@ from loaders.loaders import load_file
 
 class TestLoaders(unittest.TestCase):
     def test_load_file(self):
-        file_path = Path('data/sample.txt')
+        file_path = Path('tests/data/sample.txt')
         expected_output = ['1', '2', '3', '4', '5']
         self.assertEqual(load_file(file_path), expected_output)
 
