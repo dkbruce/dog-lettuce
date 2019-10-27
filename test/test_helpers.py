76b7e13994248c8190bfb721d2acd0bade2d9c6b
Dylan Bruce <dylankbruce@gmail.com>
2019-10-27 18:52:57 -0400

    refactor

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -2,7 +2,7 @@ import unittest
 
 import pandas as pd
 
-from process.helpers import dict_to_df
+from process.helpers import dict_to_df, increment_count
 from pandas.testing import assert_frame_equal
 
 
@@ -13,6 +13,13 @@ class TestLoaders(unittest.TestCase):
         val_name = 'b'
         real_output = pd.DataFrame(data={'a': ['first', 'second'], 'b': [3, 4]})
         assert_frame_equal(real_output, dict_to_df(input_dict, key_name, val_name))
+
+    def test_increment_count(self):
+        test = {'a': 1}
+        test = increment_count(test, 'a')
+        self.assertEqual(test, {'a': 2})
+        test = increment_count(test, 'b')
+        self.assertEqual(test, {'a': 2, 'b': 1})
 
 
 if __name__ == '__main__':
