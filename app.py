55493653c844ef158283717603c4bb57314ff2d2
dkbruce <dylankbruce@gmail.com>
2019-10-30 19:05:52 -0400

    weoiru

--- app.py
+++ app.py
@@ -29,15 +29,14 @@ def home():
         scores_df.columns = query.keys()
         time = scores_df['time'].iloc[0]
         scores_df = scores_df[['user_id', 'challenge_score', 'username', 'maps_played']]
-        visible_cols = ['user_id', 'challenge_score', 'username']
-        usernames = scores_df['username'].values.tolist()
+        visible_cols = ['user_id', 'challenge_score', 'username', 'maps_played']
         trans.commit()
     except:
         trans.rollback()
         raise
     c.close()
     return render_template('home.html', column_names=visible_cols, row_data=list(scores_df.values.tolist()),
-                           last_update=time, usernames=usernames, zip=zip)
+                           last_update=time, zip=zip)
 
 
 if __name__ == "__main__":
