e963ca660359e46b868e1b8430ff5a7570ccd049
dkbruce <dylankbruce@gmail.com>
2019-10-29 21:13:46 -0400

    die

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ beatmaps_file = 'challenge0001.txt'
 
 if __name__ == '__main__':
     user_count = count_top_scores(beatmaps_file, challenge0001_agg, 8, verbose=True, to_csv=True)
-    with open('data/last_update', 'r') as f:
+    with open('data/last_update.txt', 'r') as f:
         lines = f.readlines()
     print(lines)
