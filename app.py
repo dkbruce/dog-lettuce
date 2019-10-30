00f1de0ee7e2a1da973c80c4573ca6bfa85c5a8c
dkbruce <dylankbruce@gmail.com>
2019-10-29 22:02:42 -0400

    shit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-import psycopg2
+import sqlalchemy
 
 import pandas as pd
 
@@ -16,7 +16,7 @@ from main import beatmaps_file
 app = Flask(__name__)
 
 DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = sqlalchemy.create_engine(DATABASE_URL)
 c = conn.cursor()
 
 
